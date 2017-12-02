844da78794e77f4d799b4927a69c1a918e754f82
Alexander Crompton <alexcromp@gmail.com>
2017-12-02 18:18:57 +0100

    Add more vocab / Refactor the drills (#5)

--- src/components/DrillContainer.js
+++ src/components/DrillContainer.js
@@ -26,61 +26,59 @@ export default class DrillContainer extends React.Component {
   handleVocabSubmission(response) {
     response = response.trim();
     const answerIndex = this.state.wordsToComplete.map(wordPair => wordPair[0]).indexOf(response);
-    if (this.props.drillType === 'withPrompts') {
-      this.handleVocabSubmissionWithPrompts(response, answerIndex);
-    } else if (this.props.drillType === 'withoutPrompts') {
-      this.handleVocabSubmissionWithoutPrompts(response, answerIndex);
+    switch (this.props.drillType) {
+      case 'withPrompts':
+        this.handleVocabSubmissionWithPrompts(response, answerIndex);
+        break;
+      case 'withoutPrompts':
+        this.handleVocabSubmissionWithoutPrompts(response, answerIndex);
+        break;
     }
   }
 
   handleVocabSubmissionWithPrompts(response, answerIndex) {
-    const isAnswerCorrect = !((answerIndex === -1) || ((this.props.drillType === 'withPrompts') && (this.state.wordsToComplete[answerIndex][0] !== response)));
-    this.updateStateAfterAnswer(answerIndex, isAnswerCorrect);
-  }
-
-  handleVocabSubmissionWithoutPrompts(response, answerIndex) {
-    if (answerIndex === -1) { // Wrong answer!
-      return;
-    } else {
-      this.updateStateAfterAnswer(answerIndex, true);
-    }
-  }
-
-  updateStateAfterAnswer(answerIndex, isAnswerCorrect) {
+    const isAnswerCorrect = !((answerIndex === -1) || (this.state.wordsToComplete[answerIndex][0] !== response));
     const newState = {};
     const updatedWordsToComplete = this.state.wordsToComplete.filter((wordPair, index) => index !== answerIndex);
     const completedWords = this.state.wordsAlreadyCompleted.slice();
     if (isAnswerCorrect) {
       const answer = this.state.wordsToComplete.slice(answerIndex, answerIndex + 1);
       completedWords.unshift(answer[0]);
-    } else { // We know this is always a "withPrompts" drill
+    } else {
       updatedWordsToComplete.shift();
       const incorrectWords = this.state.incorrectWords.slice();
       incorrectWords.unshift(this.state.wordsToComplete[0]);
       completedWords.unshift(this.state.wordsToComplete[0]);
       Object.assign(newState, { incorrectWords });
     }
-
-    if (this.props.drillType === 'withPrompts') {
-      let nextPrompt = ' ';
-      if (this.state.wordsToComplete.length > 1) {
-        nextPrompt = this.state.wordsToComplete[1][1];
-      }
-      Object.assign(newState, { prompt: nextPrompt });
-    } else {
-      if (this.state.wordsToComplete.length === 1) {
-        Object.assign(newState, { prompt: ' ' })
-      }
+    let nextPrompt = ' ';
+    if (this.state.wordsToComplete.length > 1) {
+      nextPrompt = this.state.wordsToComplete[1][1];
     }
+    Object.assign(newState, { 
+      prompt: nextPrompt, 
+      wordsToComplete: updatedWordsToComplete,
+      wordsAlreadyCompleted: completedWords,
+    });
+    this.setState(newState);   
+  }
 
-
+  handleVocabSubmissionWithoutPrompts(response, answerIndex) {
+    if (answerIndex === -1) { return };
+    const newState = {};
+    const updatedWordsToComplete = this.state.wordsToComplete.filter((wordPair, index) => index !== answerIndex);
+    const completedWords = this.state.wordsAlreadyCompleted.slice();
+    const answer = this.state.wordsToComplete.slice(answerIndex, answerIndex + 1);
+    completedWords.unshift(answer[0]);
+    if (this.state.wordsToComplete.length === 1) {
+        Object.assign(newState, { prompt: ' ' })
+    }
     Object.assign(newState, {
       wordsToComplete: updatedWordsToComplete,
       wordsAlreadyCompleted: completedWords,
     });
     this.setState(newState);
   }
-
 
   handleRestart() {
     const unshuffledWordList = this.state.wordsToComplete.concat(this.state.wordsAlreadyCompleted);
@@ -136,7 +134,7 @@ export default class DrillContainer extends React.Component {
           <div style={{ marginRight: '12.5%', clear: 'both', float: 'right', padding: 10 }}>
             {this.state.wordsToComplete.length > 0 &&
               <Button id="showAnswers" clickHandler={this.handleSeeAllAnswers} text="See all answers" />
-						}
+						} 
             <Button id="return" clickHandler={this.handleReturnToMenu} text="Return to menu" />
             <Button id="restart" clickHandler={this.handleRestart} text="Restart" autoRevertColorChange={true} />
 
